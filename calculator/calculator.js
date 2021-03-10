76ce8e473e1263fe64299719f7b439b68625bd35
ako <andychenko@gmail.com>
2021-03-10 16:52:23 +0800

    complete calculator and palindrome exercises

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,52 @@
-function add () {
-	
+function add (a, b) {
+	return sum = a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return difference = a - b;
 }
 
-function sum () {
-	
+function sum (array) {	
+	return array.reduce((acc, i) => acc + i, 0);
+};
+
+function multiply (array) {
+	return array.length 
+	? array.reduce((acc, n) => acc * n) 
+	: 0;
 }
 
-function multiply () {
-	
+function power(i, e) {
+	return i ** e;
+	// var. 2: Math.pow(i, e);	
 }
 
-function power() {
-	
+function recursiveFactorial (n) {
+	if (n==0){
+		return 1;
+	}
+	return n * recursiveFactorial (n-1);
 }
 
-function factorial() {
-	
-}
+// var. 2
+// function factorial(n) {
+// 	if (n == 0) return 1;
+// 	let product = 1;
+// 	for (let i = n; i > 0; i--) {
+// 		product *= i;
+// 	}
+// 	return product;
+// }
+
+// var. 1 
+// 	if (n==0) return 1;
+// 	else {
+// 		for(let i = n - 1; i > 0; i--) {
+// 			n = n * i;
+// 		}
+// 		return n;
+// 	}
+// }
 
 module.exports = {
 	add,
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,60 @@
-const palindromes = function() {
 
+let palindromes = function(rawString) {
+
+    // split the string into characters, remove spaces and punctuation
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
+    let rawLetters = rawString.split('');
+    let cleanLetters = rawLetters.filter(function(letter) {
+        return punctuation.indexOf(letter) === -1;
+    });
+    let cleanString = cleanLetters.join('');
+    cleanString = cleanString.toLowerCase();
+    // console.log(cleanString);
+
+    // find the centermost character by dividing by 2
+    let sln = cleanString.length;
+    // console.log(sln);
+    let hsln = sln/2 
+    // console.log(hsln);
+
+    let front = "";
+    let back = ";"
+    // if result is not integer, then round up to find center character
+    if (!(Number.isInteger(hsln))) {
+        index = hsln - 0.5;
+        front = cleanString.slice(0, index + 1);
+        back = cleanString.slice(index);
+        // console.log(front, back);
+    } else if (Number.isInteger(hsln)){
+        index = hsln; 
+        front = cleanString.slice(0, index);
+        back = cleanString.slice(index);
+        // console.log(front, back);
+    }
+    // console.log(`Index is ${index}`)
+
+    // slice the string in half
+    // let front = cleanString.slice(0, index + 1);
+    // let back = cleanString.slice(index);
+    // console.log(front, back);
+
+    // reverse the first half of the string
+    function reverseString(str) {
+        return str.split('').reverse().join('');
+    }
+
+    front = reverseString(front);
+    // console.log(front);
+    // console.log(back);
+    // compare strings
+    if (front == back) return true;
+    else if (front !== back){
+        return false;
+    }
 }
+// if all characters match, is palindrome (true)
+// if any character doesn't match, is not (false)
+
+palindromes("Animal loots foliated detail of stool lamina.")
 
 module.exports = palindromes
