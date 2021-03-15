3d45ad6964c3a03105795cbe362b22424142f08f
Anchen Ko <70150536+0ako@users.noreply.github.com>
2021-03-15 15:23:34 +0800

    complete

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-let findTheOldest = function() {
+// attempt 2:
+let findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return currentAge > oldestAge ? current : oldest;
+    })
+}
 
+let getAge = function(birth, death) {
+    if(!death) death = new Date().getFullYear()
+    return death - birth; 
+}
+
+// copied out: 
+    return array.reduce((oldestPerson, currentPerson) =>   { // we operate on these 2 params
+        let oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath) // use getAge helper function
+        let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)  // ditto
+        return oldestAge < currentAge ? currentPerson : oldestPerson // use ternary to get the oldestPerson
+    })
+}
+
+let getAge = function(birthYear, deathYear) {
+    if (!deathYear) deathYear = new Date().getFullYear() // if deathYear is falsy, use today's date
+    return deathYear - birthYear
 }
 
 module.exports = findTheOldest
